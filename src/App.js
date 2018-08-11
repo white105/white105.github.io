d5312814269721ccd72decea910c6f68ab3a7636
Nicolas White <nw11@illinois.edu>
2018-08-11 01:43:30 -0400

    add social footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,23 @@ import Projects from './components/Projects/index.js'
 import Research from './components/Research/index.js'
 import Resume from './components/Resume/index.js'
 
+import FontAwesome from 'react-fontawesome';
+
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+
+    }
+
+    this.navigateSocial = this.navigateSocial.bind(this)
+  }
+
+  navigateSocial(event) {
+    console.log('social network', event)
+  }
 
   render() {
     return (
@@ -32,6 +48,37 @@ class App extends Component {
 
         </div>
       </Router>
+
+      <div className='siteFooter'>
+        <div className='fontAwesomeIconDiv'>
+
+          <a className='iconLink' href='https://github.com/white105'>
+            <FontAwesome
+              className='fontAwesomeIcon'
+              name='github'
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
+          </a>
+
+          <a className='iconLink' href='https://twitter.com/nick_w_white'>
+            <FontAwesome
+              className='fontAwesomeIcon'
+              name='twitter'
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
+          </a>
+
+          <a className='iconLink' href='https://www.linkedin.com/in/nicholas-w-white/'>
+            <FontAwesome
+              className='fontAwesomeIcon'
+              name='linkedin'
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
+          </a>
+
+        </div>
+      </div>
+
       </div>
     )
   }
