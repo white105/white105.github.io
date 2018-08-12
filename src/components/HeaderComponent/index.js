e3256df06d7415a9f4563d92afb7900a3fe7e112
Nicolas White <nw11@illinois.edu>
2018-08-11 21:24:26 -0400

    begin mobile version

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -8,12 +8,14 @@ class HeaderComponent extends Component {
       <div className='headerDiv'>
         <h1><Link to='/' style={{'color' : 'black', 'textAlign' : 'center'}}>Nicholas White</Link></h1>
 
-        <ul id='mainHeaderSubList'>
-          <li><Link to='/resume' className='headerTab'>Resume</Link></li>
-          <li><Link to='/projects' className='headerTab'>Projects</Link></li>
-          <li><Link to='/research' className='headerTab'>Research</Link></li>
-          <li><Link to='/articles' className='headerTab'>Articles</Link></li>
-        </ul>
+        <div className='mainHeaderSubListContainer'>
+          <ul id='mainHeaderSubList'>
+            <li><Link to='/resume' className='headerTab'>Resume</Link></li>
+            <li><Link to='/projects' className='headerTab'>Projects</Link></li>
+            <li><Link to='/research' className='headerTab'>Research</Link></li>
+            <li><Link to='/articles' className='headerTab'>Articles</Link></li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,10 +4,10 @@ class Home extends Component {
 
   render() {
     return (
-      <div style={{'display' : 'flex', 'flexDirection': 'column', 'justifyContent': 'center', 'marginTop' : '5rem'}}>
+      <div className='homePageContainer'>
 
-        <h4 style={{'textAlign': 'center'}}>Hey there, welcome to my personal website!</h4>
-        <p style={{'textAlign': 'center', 'width' : '30rem', 'margin': 'auto'}}>My name is Nick. I'm a student at the University at Buffalo pursuing a bachelors degree
+        <h4 className='homePageHeader'>Hey there, welcome to my personal website!</h4>
+        <p className='homePageParagraph'>My name is Nick. I'm a student at the University at Buffalo pursuing a bachelors degree
           in computer science and engineering. If you'd like to learn more about me, please feel free to navigate through the site
         using the tabs above. For more information you can contact me through the social links below.</p>
 
