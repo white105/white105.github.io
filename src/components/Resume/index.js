03fce56b68483b7d790ae2e7f494b6a978dce1f2
Nicolas White <nw11@illinois.edu>
2018-08-11 23:19:21 -0400

    add responsive resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,8 +5,8 @@ class Resume extends Component {
   render() {
 
     return (
-      <div>
-        <iframe src="https://drive.google.com/file/d/1lyT_BXSbZLESN41KgN84D9shoZIr50E2/preview" frameBorder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" id='resumeFrame' scrolling="no"></iframe>
+      <div id='resumeDiv'>
+        <iframe className='resume' src="https://drive.google.com/file/d/1lyT_BXSbZLESN41KgN84D9shoZIr50E2/preview" frameBorder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" id='resumeFrame' scrolling="no"></iframe>
       </div>
     );
   }
