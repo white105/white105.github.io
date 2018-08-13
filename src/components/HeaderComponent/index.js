c9bc6a5eb196a4178a6a4b574476568516ff8971
Nicolas White <nw11@illinois.edu>
2018-08-13 01:01:39 -0400

    fix mobile header

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -8,14 +8,12 @@ class HeaderComponent extends Component {
       <div className='headerDiv'>
         <h1><Link to='/' id='headerLink'>Nicholas White</Link></h1>
 
-        <div className='mainHeaderSubListContainer'>
-          <ul id='mainHeaderSubList'>
-            <li><Link to='/resume' className='headerTab'>Resume</Link></li>
-            <li><Link to='/projects' className='headerTab'>Projects</Link></li>
-            <li><Link to='/research' className='headerTab'>Research</Link></li>
-            <li><Link to='/articles' className='headerTab'>Articles</Link></li>
-          </ul>
-        </div>
+        <ul id='mainHeaderSubList'>
+          <li><Link to='/resume' className='headerTab'>Resume</Link></li>
+          <li><Link to='/projects' className='headerTab'>Projects</Link></li>
+          <li><Link to='/research' className='headerTab'>Research</Link></li>
+          <li><Link to='/articles' className='headerTab'>Articles</Link></li>
+        </ul>
       </div>
     );
   }
