7a6eeb449d5d9c7e1e2ed1497ad46fafecc34d57
Nicolas White <nw11@illinois.edu>
2018-08-13 02:12:33 -0400

    add github projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { UserCard, RepoCard } from 'react-github-cards';
+import 'react-github-cards/dist/default.css';
 
 class Projects extends Component {
 
   render() {
     return (
-      <div id='resumeDiv'>
-        <iframe className='resume' src="https://drive.google.com/file/d/1lyT_BXSbZLESN41KgN84D9shoZIr50E2/preview" frameBorder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" id='resumeFrame' scrolling="no"></iframe>
+      <div className='projectsDiv'>
+        <RepoCard username="white105" repo="csc-395" className='githubRepo' />
+        <RepoCard username="white105" repo="Rubric-Calculator" className='githubRepo' />
       </div>
     );
   }
