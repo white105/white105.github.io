067b6554b5b39735ddc0d2a5f7d0295238ae526b
Nicolas White <nw11@illinois.edu>
2018-11-24 18:57:29 -0500

    kind of fix mobile responsive header

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -1,22 +1,102 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 
 class HeaderComponent extends Component {
 
-  render() {
-    return (
-      <div className='headerDiv'>
-        <h1><Link to='/' id='headerLink'>Nicholas White</Link></h1>
+  constructor(props) {
+    super(props)
+    this.state = {
+      isDesktop: false,
+      dropdown: false
+    }
 
-        <ul id='mainHeaderSubList'>
-          <li><Link to='/resume' className='headerTab'>Resume</Link></li>
-          <li><Link to='/projects' className='headerTab'>Projects</Link></li>
-          <li><Link to='/research' className='headerTab'>Research</Link></li>
-          <li><Link to='/hackathons' className='headerTab'>Hackathons</Link></li>
-          <li><Link to='/articles' className='headerTab'>Articles</Link></li>
-        </ul>
-      </div>
-    );
+    this.updatePredicate = this.updatePredicate.bind(this);
+    this.dropdownHeader = this.dropdownHeader.bind(this)
+  }
+
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 600 });
+  }
+
+  dropdownHeader() {
+    let dropdown = this.state.dropdown
+    let headerDiv = document.getElementById("headerDiv")
+    if (dropdown) {
+      headerDiv.style.height = '5rem'
+    } else {
+      headerDiv.style.height = '14rem'
+    }
+    this.setState({ dropdown : !dropdown })
+  }
+
+  render() {
+    const isDesktop = this.state.isDesktop;
+    if (isDesktop) {
+      return (
+        <div className='headerDiv'>
+          <h1><Link to='/' id='headerLink'>Nicholas White</Link></h1>
+
+          <ul id='mainHeaderSubList'>
+            <li><Link to='/resume' className='headerTab'>Resume</Link></li>
+            <li><Link to='/projects' className='headerTab'>Projects</Link></li>
+            <li><Link to='/research' className='headerTab'>Research</Link></li>
+            <li><Link to='/hackathons' className='headerTab'>Hackathons</Link></li>
+            <li><Link to='/articles' className='headerTab'>Articles</Link></li>
+          </ul>
+        </div>
+      );
+    } else {
+      if (this.state.dropdown) {
+        return (
+          <div className='headerDiv' id='headerDiv'>
+            <div className='headerTopDiv'>
+
+              <FontAwesome
+                onClick={this.dropdownHeader}
+                className='dropDownIcon'
+                name='arrow-circle-up'
+                style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+              <h1><Link to='/' id='headerLink'>Nicholas White</Link></h1>
+            </div>
+
+            <ul id='mainHeaderSubList'>
+              <li><Link to='/resume' className='headerTab'>Resume</Link></li>
+              <li><Link to='/projects' className='headerTab'>Projects</Link></li>
+              <li><Link to='/research' className='headerTab'>Research</Link></li>
+              <li><Link to='/hackathons' className='headerTab'>Hackathons</Link></li>
+              <li><Link to='/articles' className='headerTab'>Articles</Link></li>
+            </ul>
+          </div>
+        );
+      } else {
+        return (
+          <div className='headerDiv' id='headerDiv'>
+
+            <div className='headerTopDiv'>
+              <FontAwesome
+                onClick={this.dropdownHeader}
+                className='dropDownIcon'
+                name='arrow-circle-down'
+                style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+
+              <h1><Link to='/' id='headerLink'>Nicholas White</Link></h1>
+            </div>
+          </div>
+        );
+      }
+    }
   }
 }
 
